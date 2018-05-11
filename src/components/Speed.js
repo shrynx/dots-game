e95db3661667a3aeb5f7796b90ad29a7b207ed71
Shriyans <shriyansbhatnagar@gmail.com>
2018-05-11 22:12:58 +0900

    added speed component

---
+++ src/components/Speed.js
@@ -0,0 +1,12 @@
+// @flow
+import styled from 'styled-components';
+import { PURPLE } from '../styles/colors';
+
+const Speed = styled.span`
+  font-size: 12px;
+  text-align: center;
+  font-weight: 600;
+  color: ${PURPLE};
+`;
+
+export default Speed;
