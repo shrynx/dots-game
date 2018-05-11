d2ee79c4204a441e6839bf2bc44e0aa5a1dbc27b
Shriyans <shriyansbhatnagar@gmail.com>
2018-05-11 22:12:41 +0900

    added score component

---
+++ src/components/Score.js
@@ -0,0 +1,13 @@
+// @flow
+import styled from 'styled-components';
+import { PURPLE } from '../styles/colors';
+
+const Score = styled.div`
+  font-size: 32px;
+  font-weight: 600;
+  text-align: center;
+  color: ${PURPLE};
+  margin-bottom: 10px;
+`;
+
+export default Score;
